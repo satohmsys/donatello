6bb24cf528fdaac7ba60247680e5c75a5f5645b7
佐藤正康 <satohmsys@gmail.com>
2017-12-26 01:31:07 +0900

    index pc/spmacモダンブラウザ確認済み

--- donatello.com/common/js/common.js
+++ donatello.com/common/js/common.js
@@ -4,27 +4,77 @@
 
 var $w = $(window),
 	$wH = $w.height(),
+	$wW = $w.width(),
 	$effectTarget = $('.ef'),
 	$scrollVal = 0,
-	$scrollOffset = $wH / 2;
+	$distance = 0,
+	$scrollOffset = $wH / 1.1;
 
 $w.on( 'load scroll resize' , function( e ){
 	
 	if(! $effectTarget.length ) return;
 
-	$scrollVal = $scrollOffset + $w.scrollTop();
+	$scrollVal = $w.scrollTop();
+	$distance = $scrollOffset + $scrollVal;
+	$wW = $w.width();
 
 	$.each( $effectTarget, function( e ){
 		var $self = $( this ),
 			$self_offsetTop = $self.offset().top;
 
-		if( $self_offsetTop < $scrollVal ){
+		if( $self_offsetTop < $distance ){
 			$self.css({
 				'transition-delay' : $self.data('delay') + 's'
 			}).addClass( 'doEffect' );
 		}
 	});
+});
+
+
+
+/**
+* navtoggle
+*/
+
+var $toggle = $('.navToggle');
+
+$toggle.on( 'click', function( e ){
+	e.stopPropagation();
+	$body = $('body');
+
+	if( $body.hasClass( 'navOpen' ) || 768 < $wW ){
+		$body.removeClass( 'navOpen' )
+	} else if(! $body.hasClass('navOpen') ) {
+		$body.addClass( 'navOpen' );
+	} else {
+		$body.addClass( 'navOpen' );
+	}
 })
+
+
+
+/**
+* fixed header 
+*/
+var $timer = true,
+	$siteHeader = $('.siteHeader');
+
+$w.on( 'scroll', function( e ){
+
+	if( $timer ){
+		$timer = false;
+		setTimeout( function( e ){
+			if( 300 < $scrollVal ){
+				$siteHeader.addClass( 'scrolled' );
+			} else {
+				$siteHeader.removeClass( 'scrolled' );
+			}
+			$timer = true;
+			return $timer;
+		}, 400 );	
+	}
+});
+
 
 
 /**
